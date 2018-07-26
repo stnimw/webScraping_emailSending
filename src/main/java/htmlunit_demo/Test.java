a497ed4606bbc7f271c9441b93c92939b5361b89
Wang MingTzu <stnimw@gmail.com>
2018-07-26 10:51:42 +0800

    Edited comments

--- src/main/java/htmlunit_demo/Test.java
+++ src/main/java/htmlunit_demo/Test.java
@@ -12,22 +12,22 @@ public class Test {
 		TimerTask task = new TimerTask() {
 			@Override
 			public void run() {
-				String divisionNo = "003";// Hsinchu
-				String depNo = "CE100";// Obstetrics and Gynecology
-				String roomNo = "032";// 劉惠珊 LIU HUI SHAN
+				String divisionNo = "003";// Hospital division (003:Hsinchu)
+				String depNo = "CE100";// Department (CE100: Obstetrics and Gynecology)
+				String roomNo = "032";// Room number (032: room of physician 劉惠珊 LIU HUI SHAN)
 				String myNO = "3";
 				String notifiedNO = "4";
 				String pageXml = serviceProgress.getProgressEn(divisionNo, depNo);
 				MsgVO vo = serviceProgress.extractDataEn(pageXml, roomNo);
 				if (vo.getCurrNo() != null && vo.getCurrNo().equals(notifiedNO)) {
-					String reciever = "takenon5@gmail.com";
+					String receiver = "reciever@gmail.com";//Receiver's email
 					String title = "Appointment time is close";
 					String msg = 
 							"Your Number: " + myNO 
 							+ "<br>Current Number: " + vo.getCurrNo() 
 							+ "<br>Room Number: "+ vo.getRoomNo() 
 							+ "<br>Physician: " + vo.getPhysician();
-					final Sender sender = new Sender(reciever, title, msg);
+					final Sender sender = new Sender( receiver, title, msg);
 					sender.start();
 					timer.cancel();
 					System.out.println("Timer cancel");
